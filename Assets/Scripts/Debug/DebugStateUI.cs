3adda2bd59df024fce439355cc5111d18d7b52b7
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-08-06 21:21:21 +0300

    Add Debug state UI

---
+++ Assets/Scripts/Debug/DebugStateUI.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rampart.Remake { 
+
+    public class DebugStateUI : MonoBehaviour {
+
+        [SerializeField]
+        Text _player1TextUI;
+
+        [SerializeField]
+        Text _player2TextUI;
+
+        [SerializeField]
+        Text _gameStateTextUI;
+
+        [SerializeField]
+        Text _connectedTextUI;
+
+        GameManager _gameManager;
+
+        void Start() {
+            _gameManager = GameManager.instance;
+            _player1TextUI.text = "";
+            _player2TextUI.text = "";
+        }
+
+        void Update() {
+            _gameStateTextUI.text = _gameManager.GetGameMode().ToString();
+            if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom) {
+                _connectedTextUI.text = PhotonNetwork.IsConnected.ToString();
+                Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
+                _player1TextUI.text = players[0].NickName;
+                _player2TextUI.text = players[1].NickName;
+            } else {
+                _connectedTextUI.text = PhotonNetwork.IsConnected.ToString();
+            }
+        }
+
+    }
+}
