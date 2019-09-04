735d3ca93000557f438ed703f81a38fa546c76a8
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2019-09-04 22:17:27 +0300

    Destroy wall on bullet hit

---
+++ Assets/Scripts/Wall/DestroyWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Rampart.Remake { 
+    public class DestroyWall : MonoBehaviour {
+
+        [SerializeField] string collisionTag = "";
+
+        void OnTriggerEnter(Collider other) {
+            if (other.tag == collisionTag) {
+                Debug.Log("Trigger " + other.tag);
+                this.DestroyWallPiece();
+            }
+        }
+
+        void DestroyWallPiece() {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
